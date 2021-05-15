20a48dfc9a829d3fde59794c53dfd0cdaa335c40
Abhishek Vijay <abhivijay96@gmail.com>
2021-05-15 17:16:21 -0400

    unique data points generator

--- dataset/count_unique.py
+++ dataset/count_unique.py
@@ -7,4 +7,8 @@ with open(sys.argv[1], 'r') as read_file:
         line = line.strip()
         seen_lines.add(line)
 
-print('Unique number of data points in', sys.argv[1], len(seen_lines))
+# print('Unique number of data points in', sys.argv[1], len(seen_lines))
+
+with open(sys.argv[1], 'w') as write_file:
+    for line in seen_lines:
+        write_file.write(line + '\n')
